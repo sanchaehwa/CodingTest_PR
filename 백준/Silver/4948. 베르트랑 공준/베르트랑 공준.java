c8b81db7c84956ff76864954841aeb56eb34317f
HwaYoung <110398814+sanchaehwa@users.noreply.github.com>
2024-10-29 12:08:10 +0900

    [Silver II] Title: 베르트랑 공준, Time: 252 ms, Memory: 20312 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //List
+        //입력받은 숫자 저장할 리스트
         List<Integer> inputs = new ArrayList<>();
 
 
         while (true) {
             int N = sc.nextInt();
-            //0일때는 예외처리
-            if (N == 0) break;
-            inputs.add(N);
+            if (N == 0) break; //0입력하면 종료
+            inputs.add(N); //0이 아니면 List
         }
 
         //한줄에 하나의 데이터만 처리하게 (1을 입력하면 1보다 크고 2보다 작거나 같은 소수의 개수를 출력하게, 다른 수를 입력하면 그 수에 맞는)
         int maxN = inputs.stream().max(Integer::compare).orElse(0);
+        //2N 까지 소수 여부 판별 배열
         boolean[] isPrime = new boolean[2 * maxN + 1];
-        //소수
+        //배열 초기화
         for (int i = 2; i <= 2 * maxN; i++) {
             isPrime[i] = true;
         }
-        //소수가아닌수
+        //소수가 아닌수를 true -> false
         for (int i = 2; i * i <= 2 * maxN; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= 2 * maxN; j += i) {
@@ -32,11 +32,11 @@ public class Main{
             }
         }
 
-
+        //소수 개수 계산 출력
         for (int N : inputs) {
             int count = 0;
             for (int i = N + 1; i <= 2 * N; i++) {
-                if (isPrime[i]) {
+                if (isPrime[i]) { //소수일우
                     count++;
                 }
             }
